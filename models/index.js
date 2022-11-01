14bfabbb7693a0bf9fae93d0b60960e7103b328e
William Chalk <william@printedproducts.com>
2022-11-01 15:44:25 -0400

    added delete functionality

--- models/index.js
+++ models/index.js
@@ -8,18 +8,22 @@ User.hasMany(Post, {
 
 Post.belongsTo(User, {
   foreignKey: "user_id",
+  onDelete: 'SET NULL'
 });
 
 Comment.belongsTo(User, {
   foreignKey: "user_id",
+  onDelete: 'SET NULL'
 });
 
 Comment.belongsTo(Post, {
   foreignKey: "post_id",
+  onDelete: 'SET NULL'
 });
 
 User.hasMany(Comment, {
   foreignKey: "user_id",
+  onDelete: 'SET NULL'
 });
 
 Post.hasMany(Comment, {
